31317b07c0ccb41a7c0a473012410dbd9de81139
JonatanDrysen <jojodrysen@gmail.com>
2024-03-06 10:39:29 +0100

    move ChannelPage code to separate component & make useEffect update on channelId change

--- src/pages/ChannelPage.tsx
+++ src/pages/ChannelPage.tsx
@@ -1,34 +1,9 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getChannelPrograms } from "../api/getChannelPrograms";
-import { ChannelProgramsResponse } from "../interfaces";
+import { Channel } from "../components/Channel";
 
 export function ChannelPage() {
-    const { channelId } = useParams()
-    const [channelData, setChannelData] = useState<ChannelProgramsResponse | null>(null)
-
-    useEffect(() => {
-        const id: number = Number(channelId)
-
-        const fetchChannel = async (id: number) => {
-            const response = await getChannelPrograms(id)
-            setChannelData(() => response)
-        }
-        fetchChannel(id)
-    }, [])
-
-    return (
-        <>
-            <h2>{channelData && channelData.programs[0].channel.name}</h2>
-            <div>
-                <ul>
-                    {channelData && channelData.programs.map((program) => {
-                        return (
-                            <li key={program.id}>{program.name}</li>
-                        )
-                    })}
-                </ul>
-            </div>
-        </>
-    )
+  return (
+    <>
+        <Channel />
+    </>
+  )
 }
